6a7820630b9dd05906137459cb28356a3a9a797d
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2024-09-11 17:22:05 -0300

    Tabu search in development

---
+++ Functions/TabuSearch.py
@@ -0,0 +1,2 @@
+def ObjFun():
+    print("Hi")
--- Main.py
+++ Main.py
@@ -7,11 +7,11 @@ import numpy as np
 # Path from the workspace.
 sys.path.append(os.path.join(os.path.dirname(__file__), 'functions'))
 from ReadTSP import ReadTsp # type: ignore
-
+from TabuSearch import ObjFun  # type: ignore
 
 ########## Procedure ##########
 
-# Obtain trainning TSP's instances.
+""" # Obtain trainning TSP's instances.
 Path_T = "InstanciasTSP/Parametrizacion"
 Content = os.listdir(Path_T)
 files = []
@@ -22,4 +22,6 @@ for file in Content:
 # Reading TSP instances.
 for filename in files:
     ReadTsp(filename)
+"""
 
+ObjFun()
